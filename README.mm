c4032dbb45fc2538e3fd78ec90c8452161f8ad91
Ryan Tulabing <tulabingryan@users.noreply.github.com>
2021-04-02 23:52:15 +1300

    Update and rename README.txt to README.mm

---
+++ README.mm
@@ -0,0 +1,8 @@
+# Django Web App for Electrical Engineering exams 
+Author: Ryan Tulabing       Date: August 2016
+
+# Running the project
+- Activate the virtual environment venv.
+- Source codes are inside the folder src
+
+
--- README.txt
+++
@@ -1,6 +0,0 @@
-#This is a project for Electrical Engineering exams.
-# Author: Ryan Tulabing
-# Date: August 2016
-
-
-
